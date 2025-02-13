55bc8263f5866a039f62bb1b8402bab5cd127e7e
TanvirSajib <tanvir.sajib.programmer@gmail.com>
2025-02-13 14:43:29 +0600

    2.3 wp_localize_script added

--- files/ajax_basic.php
+++ files/ajax_basic.php
@@ -3,12 +3,24 @@
 add_action( 'wp_enqueue_scripts', 'tanvir_ajax_script' );
 
 function tanvir_ajax_script(){
+    
     wp_enqueue_script(
         'tanvir-ajax-script',
         plugin_dir_url(__FILE__) . '../js/tanvir-ajax-script.js',
         array( 'jquery' ),
         null,
         true
+    );
+
+
+     // Localize script to pass PHP variables to javascript
+     wp_localize_script(
+        'tanvir-ajax-script',
+        'tanvir_ajax_object',
+        array(
+            'ajax_url' => admin_url('admin-ajax.php'),
+            'nonce'    => wp_create_nonce('tanvir_ajax_nonce'),
+        )
     );
 
     
